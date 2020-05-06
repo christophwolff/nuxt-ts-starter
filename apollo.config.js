b8179ad1b52f8ad4e34f07fee0585b4b9422eaff
Christoph Wolff <cw@christophwolff.de>
2020-05-06 12:03:52 +0200

    Remove Global config

--- apollo.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  client: {
-    localSchemaFile: './schema.json',
-    service: {
-      name: 'br-radio',
-      url: 'https://graphql.alpha.gateway.br-master.de/graphql'
-    }
-  }
-}
